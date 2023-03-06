a27334eb39bd5d8e9a5b2646b6357693b0a145c3
Samsonlaus <127087387+Samsonlaus@users.noreply.github.com>
2023-03-06 17:39:59 +0800

    Update fund_and_withdraw.py

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -1,5 +1,27 @@
-from brownie import FundMe
-from scripts.helpful_scripts import get_account
+from brownie import FundMe, network, config, MockV3Aggregator
+from scripts.helpful_scripts import (
+    get_account,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    deploy_mocks,
+)
+
+
+def deploy_fund_me():
+    account = get_account()
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"Contract deployed to {fund_me.address}")
 
 
 def fund():
@@ -19,5 +41,6 @@ def withdraw():
 
 
 def main():
+    deploy_fund_me()
     fund()
     withdraw()
